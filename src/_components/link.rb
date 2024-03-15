0910f1a3d00f6b0c13b3161017959b2da9f1a914
Ethan Kircher <ethanmichaelk@gmail.com>
2024-03-14 21:50:13 -0500

    ensure trailing slash always

--- src/_components/link.rb
+++ src/_components/link.rb
@@ -1,6 +1,6 @@
 class Link < Bridgetown::Component
   def initialize(href:, class: nil, relative: true, **props)
-    @href = href
+    @href = ensure_trailing_slash(href)
     @relative = relative
     @class = binding.local_variable_get(:class)
     @props = props
@@ -10,5 +10,21 @@ class Link < Bridgetown::Component
     return if @props.empty?
 
     @props.map { |k, v| "#{k.to_s.dasherize}=\"#{v}\"" }.join(" ")
+  end
+
+  def ensure_trailing_slash(href)
+    return href if href == '/'
+
+    href, query_string = href.split('?')
+
+    href = if href.ends_with?('/')
+      href
+    else
+      "#{href}/"
+    end
+
+    return "#{href}?#{query_string}" if query_string.present?
+
+    href
   end
 end
